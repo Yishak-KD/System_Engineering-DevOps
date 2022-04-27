f2be10ce943eb98b73ad6278e896e9ad39a9c7c1
root <Yishak Kidanu>
2022-04-27 03:38:02 -0700

    Script that returns number of subscribers from an API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# A script that returns number of subcribers
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function that returns number of subcribers from an API
+    """
+
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+
+    headers = {
+        'User-Agent': 'ALX',
+        'From': 'alxstud.tech'
+    }
+
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        res = r.json().get('data')
+        return res.get("subscribers")
+    else:
+        return 0
