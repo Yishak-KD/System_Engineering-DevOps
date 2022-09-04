6f83f2a138f870452ee697a0d217a617197b4e49
Yishak T. KIDANU <80487041+Yishak-KD@users.noreply.github.com>
2022-09-04 14:53:52 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Install puppet-lint version 2.5.0
-
-package { 'puppet-lint':
-   ensure   => '2.5.0',
-   provider => 'gem', 
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# create a manifest that kills a process
-
-exec { 'killmenow':
-    command  => '/usr/bin/pkill killmenow',
-    provider => 'shell',
-    returns  => [0, 1], 
-}
