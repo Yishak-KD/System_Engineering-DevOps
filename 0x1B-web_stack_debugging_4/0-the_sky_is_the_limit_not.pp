2e1685c0fc1061958d7753582429fb5711f8f114
Yishak Kidanu <yisak04@gmail.com>
2022-11-12 00:52:20 -0800

    Test how well server is featuring

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# test
+exec { 'fixer':
+    command => 'sed -i s/15/1024 /etc/default/nginx',
+    path    => '/bin',
+}
+service { 'nginx':
+    ensure    => running,
+    subscribe => Exec['fixer'],
+}
