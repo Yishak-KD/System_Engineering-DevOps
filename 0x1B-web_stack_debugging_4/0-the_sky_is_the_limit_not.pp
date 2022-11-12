b11d7fed466102cb401d229b1c5a5bf2bf236afc
Yishak Kidanu <yisak04@gmail.com>
2022-11-12 00:56:14 -0800

    Test for our webserver feature

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,11 @@
-# test
-exec { 'fixer':
-    command => 'sed -i s/15/1024 /etc/default/nginx',
-    path    => '/bin',
-}
-service { 'nginx':
-    ensure    => running,
-    subscribe => Exec['fixer'],
+# Test
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
